2aec160021c0ca1a07f2a05b62dac5f674e53533
Bin Wu <bwu11@huskers.unl.edu>
2021-08-26 23:47:23 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Bin Wu
+ * Date: 2021/08/24
+ *
+ * A simple hello world progrom
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Bin Wu, and Matthew Barrett\n");
+
+    return 0;
+}
